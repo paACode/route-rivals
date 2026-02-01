f5a4337a9560b7b309121f7d3d68c8f89c838e2d
paACode <ackermann.pascal@gmail.com>
2026-02-01 17:33:12 +0100

    Added Testmap and corresponding configs

---
+++ src/config.py
@@ -0,0 +1,14 @@
+MAP_IMAGE_PATH = "assets/map.png"
+MAP_BOUNDS = {
+    "min_lat": 47.43931,
+    "max_lat": 47.46416,
+    "min_lng": 8.12919,
+    "max_lng": 8.18584
+}
+MAP_SIZE_PIXELS = (1925, 1248)  # of your PNG
+
+# Player settings
+PLAYER_SPEED_MPS = 7  # meters per second ≈ 25 km/h
+PLAYER_COLOR = (0, 100, 255)  # Blue
+
+
